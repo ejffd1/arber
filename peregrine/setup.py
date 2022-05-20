4f6aafb2410c9f7a0fbca7aef6c5268ee29d239b
ejffd1 <105948416+ejffd1@users.noreply.github.com>
2022-05-20 16:31:40 -0400

    Add files via upload

---
+++ peregrine/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='peregrinearb',
+    version='1.8.0',
+    description='A Python library which provides several algorithms to detect arbitrage opportunities across over 132 '
+                'cryptocurrency markets in over 50 countries',
+',
+    packages=find_packages(),
+    license='MIT',
+    url='',
+)
